6fdc6805d68057c0955e119a4ed2abb8bfd8b1a1
rajivposwal <142899456+rajivposwal@users.noreply.github.com>
2025-09-15 12:40:47 +0530

    Add files via upload

---
+++ assignment5.py
@@ -0,0 +1,16 @@
+#task 1
+'''dictionary = {'Irfan' : 97,'Jafar' : 95,'Rajiv': 99,'Sumit': 84,'Aquduash' : 96, }
+Name = input("Enter the name of student: ",)
+
+if Name in dictionary:
+    print(f"{Name}'s marks : {dictionary[Name]}")
+else:
+    print(f"sorry no record founf for '{Name}'.")'''
+
+#task 2
+original_list = (100,99,299,788,24,35,47,78,36,)
+list2 = original_list[:5]
+list2_reverse =  list2[:: -1]
+
+print("exteacted list: ", list2)
+print(" reverse elements: ",list2_reverse)
